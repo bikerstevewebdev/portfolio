c075eca88c05494a3e6bb39b617912c5cf1b24b0
unknown <bikerstevefitness@gmail.com>
2018-06-07 14:09:50 -0600

    deployment fixes

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const { PORT } = process.env
 
 const app = express()
-app.use(express.static(__dirname + '/../build'))
+app.use(express.static(__dirname + '/../public'))
 
 app.listen(PORT, () => {
     console.log(`Portfolio is LIVE!!! port ${PORT}`)
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,14 +1,22 @@
 import React from 'react'
-import pic02 from '../images/pic02.jpg'
-
+import db from '../images/life-inbalance-dashboard.png'
+import workout from '../images/life-inbalance-workout.png'
 
 function Projects(props){
     return(
         <article id="work" className={`${props.article === 'work' ? 'active' : ''} ${props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
           <h2 className="major">Projects</h2>
-          <span className="image main"><img src={pic02} alt="" /></span>
-          <p>Adipiscing magna sed dolor elit. Praesent eleifend dignissim arcu, at eleifend sapien imperdiet ac. Aliquam erat volutpat. Praesent urna nisi, fringila lorem et vehicula lacinia quam. Integer sollicitudin mauris nec lorem luctus ultrices.</p>
-          <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus pharetra. Pellentesque condimentum sem. In efficitur ligula tate urna. Maecenas laoreet massa vel lacinia pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus amet feugiat tempus.</p>
+          {/* <span className="image main"><img src={pic02} alt="" /></span> */}
+          <div className="life-inbalanceimages">
+            {/* <img src={db} alt="Balanced Life Dashboard"/>
+            <img src={workout} alt="Balanced Life Example Workout"/> */}
+          </div>
+          <h3>Balanced LIFE</h3>
+          <p>Personally Developed Health and Fitness app</p>
+          <p>My background as a health and fitness expert led me to the desire to create an app that would help someone organize their health and fitness side of living so that they could balance it with the rest of their daily lives.  In originally beginning the project, the expectations were not quite as extreme as they ended up being.</p>
+          <h3>Pine Tree Clone</h3>
+          <p>Awwwards Site Winner Clone - Group Project</p>
+          <p>As a group, myself, Donnell Harris and Ricardo Carosa set out to enhance our CSS skills and strengthen our abilities to work as a team towards one common goal of a finished project by a rather rapid deadline.</p>
           {props.close}
         </article>
     )
